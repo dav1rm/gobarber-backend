a4fc62d7fab718c0fc1851d5179c328265f77107
Davi Rodrigues <davi.rodrigues@liberedu.com>
2020-05-10 00:41:33 -0300

    initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '0285e63b1d5258f0096a2c2f3632595e',
+    expiresIn: '1d',
+  },
+};
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppError from '../errors/AppError';
+import Appointment from '../models/Appointment';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const hasScheduling = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (hasScheduling) {
+      throw new AppError('This appointments is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
