0190b95f84897721e8759f13dc037cd45a46de18
Davi Rodrigues <davi.rodrigues@liberedu.com>
2020-05-24 19:26:10 -0300

    organizes structure with controllers

---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,42 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUsersDTO from "@modules/users/dtos/ICreateUsersDTO";
+
+import User from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = await this.ormRepository.findOne(id);
+
+    return findUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = await this.ormRepository.findOne({
+      where: { email },
+    });
+
+    return findUser;
+  }
+
+  public async create(userData: ICreateUsersDTO): Promise<User> {
+    const user = this.ormRepository.create(userData);
+
+    await this.ormRepository.save(user);
+
+    return  user;
+  }
+
+  public async save(user: User): Promise<User> {
+    return await this.ormRepository.save(user);
+  }
+}
+
+export default UsersRepository;
