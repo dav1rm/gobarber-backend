965198ee7f1e81ef209b52818b1fd8b1ba8ac3d9
Davi Rodrigues <davi.rodrigues@liberedu.com>
2020-06-07 00:18:57 -0300

    add schedule provider

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      date: new Date(2020, 4, 20, 14, 0, 0),
+      provider_id: 'provider',
+      user_id: '123',
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      date: new Date(2020, 4, 20, 15, 0, 0),
+      provider_id: 'provider',
+      user_id: '123',
+    });
+
+    const appointments = await listProviderAppointmentsService.execute({
+      day: 20,
+      month: 5,
+      year: 2020,
+      provider_id: 'provider',
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
